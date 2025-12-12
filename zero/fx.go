dc8d543cbdee9019694cf51a6580444388237cad
zachklingbeil <zach@zachklingbeil.com>
2025-12-12 10:13:17 -0800

    added ToString method

--- zero/fx.go
+++ zero/fx.go
@@ -24,6 +24,7 @@ type Fx interface {
 	Serve()
 	Router() *mux.Router
 	ToBytes(input string) []byte
+	ToString(input string) string
 }
 
 type fx struct {
@@ -131,28 +132,6 @@ func (f *fx) AddPath(dir string) string {
 	return prefix
 }
 
-// New method for embedded filesystem
-// func (f *fx) EmbedPath(efs embed.FS, root string) error {
-// 	return fs.WalkDir(efs, root, func(path string, d fs.DirEntry, err error) error {
-// 		if err != nil || d.IsDir() {
-// 			return err
-// 		}
-
-// 		fileData, err := efs.ReadFile(path)
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		base := filepath.Base(path)
-// 		name := base[:len(base)-len(filepath.Ext(base))]
-// 		contentType := f.getType(base, fileData)
-// 		routePath := "/" + filepath.Base(root) + "/" + name
-
-// 		f.addRoute(routePath, fileData, contentType)
-// 		return nil
-// 	})
-// }
-
 func (f *fx) getType(filename string, data []byte) string {
 	contentType := mime.TypeByExtension(filepath.Ext(filename))
 	if contentType == "" {
@@ -196,3 +175,45 @@ func (f *fx) ToBytes(input string) []byte {
 	}
 	return b
 }
+
+func (f *fx) ToString(input string) string {
+	if strings.HasPrefix(input, "http://") || strings.HasPrefix(input, "https://") {
+		resp, err := http.Get(input)
+		if err != nil {
+			return ""
+		}
+		defer resp.Body.Close()
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return ""
+		}
+		return string(b)
+	}
+	b, err := os.ReadFile(input)
+	if err != nil {
+		return ""
+	}
+	return string(b)
+}
+
+// New method for embedded filesystem
+// func (f *fx) EmbedPath(efs embed.FS, root string) error {
+// 	return fs.WalkDir(efs, root, func(path string, d fs.DirEntry, err error) error {
+// 		if err != nil || d.IsDir() {
+// 			return err
+// 		}
+
+// 		fileData, err := efs.ReadFile(path)
+// 		if err != nil {
+// 			return err
+// 		}
+
+// 		base := filepath.Base(path)
+// 		name := base[:len(base)-len(filepath.Ext(base))]
+// 		contentType := f.getType(base, fileData)
+// 		routePath := "/" + filepath.Base(root) + "/" + name
+
+// 		f.addRoute(routePath, fileData, contentType)
+// 		return nil
+// 	})
+// }
