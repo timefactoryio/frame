dd8e302e06d83264fdb00e097a90a6c5c0b7e9ff
zachklingbeil <zach@zachklingbeil.com>
2026-04-15 14:06:35 -0700

    cleanup

--- zero/main.go
+++ zero/main.go
@@ -16,15 +16,11 @@ var textHtml string
 //go:embed embed/keyboard.html
 var keyboardHtml string
 
-// //go:embed embed/layouts.json
-// var layoutsJson []byte
-
 type Zero struct {
 	SlidesTemplate string
 	TextTemplate   string
 	HomeTemplate   string
 	Keyboard       string
-	// Layouts        []byte
 }
 
 func NewZero() *Zero {
@@ -32,7 +28,6 @@ func NewZero() *Zero {
 		SlidesTemplate: slidesHtml,
 		TextTemplate:   textHtml,
 		HomeTemplate:   homeHtml,
-		// Layouts:        layoutsJson,
-		Keyboard: keyboardHtml,
+		Keyboard:       keyboardHtml,
 	}
 }
